631257651c7c298755cdc382aa26bc1e70f09505
vitikconst <vitikconst@gmail.com>
2019-01-08 21:41:06 +0300

    tula_qwestv1.1

--- index.ts
+++ index.ts
@@ -15,70 +15,57 @@ app.post('/', function (req, res) {
         userState = userStates[req.body.session.user_id];
     if (req.body.session.new === true) {
         if (!userState) {
-            userStates[req.body.session.user_id] = { commands: ["Кремль", "Набережная", "Шлем", "Все Варианты"] };
-            res.json({
-                version: req.body.version,
-                session: req.body.session,
-                response: {
-                    text: 'Привет я квет по городу Туле. Если ты что то прослушал я всегда могу повторить. Для начала квеста скажи где ты находишься?',
-                    buttons: [{ title: "Кремль" }, { title: "Набережная" }, { title: "Все варианты" }],
-                    end_session: false,
-                },
-            });
+            userState = commandDefinitions["New"];
         } else {
-            res.json({
-                version: req.body.version,
-                session: req.body.session,
-                response: {
-                    text: 'Привет рад твоему возврашению. Ты можешь продолжить квест или начать с новой точки.',
-                    buttons: [{ title: "Продолжить" }, { title: "Кремль" }, { title: "Все варианты" }],
-                    end_session: false,
-                }
-            });
-            userState = { ...userState, commands: ["Продолжить", "Кремль", "Набережная", "Шлем", "Все Варианты"] }
+            userState = commandDefinitions["Old"];
         }
     }
     else {
-        userState = readReq({ command: req.body.request.original_utterance, userState });
-        res.json(writeResp(req, userState));
-        userStates[req.body.session.user_id] = userState;
-    }
-});
+    userState = readReq({ command: req.body.request.original_utterance, userState });
+    res.json(writeResp(req, userState));
+    userStates[req.body.session.user_id] = userState;
+}});
 
 const
     commandDefinitions = {
-        "Продолжить": { message: "Возобновление сессии", commands: ["А1", "B1", "C1"] },
-        "Все варианты": { message: "Карта", commands: [], },
-        "Кремль": { command: "Кремль", message: "История кремля. Задание 1. Варианты ответов", commands: ["A1", "B1", "C1"] },
-        "A1": { command: "A", message: "Неправильно", commands: ["B1", "C1", "Подсказка1"], button: { title: "Вариант 1" } },
-        "B1": { command: "B", message: "Неправильно", commands: ["A1", "C1", "Подсказка1"], button: { title: "Вариант 2" } },
-        "C1": { command: "C", message: "Правильно.", commands: ["Следующий шаг1"], button: { title: "Вариант 3" } },
-        "Подсказка1": { command: "Подсказка про Кремль шаг 1", message: "Более детальное описание", commands: ["А1", "B1", "C1", "Следующий шаг2"] },
-        "Следующий шаг1": { command: "Следующий шаг", message: "", commands: [] },
-        "Следующий шаг2": { message: "Задание 2. Варианты ответов", commands: [{ id: "A2", text: "Вариант А" }, "B2", "C2", "Подсказка"], place: 1, step: 0 },
-        "Подсказка2": { command: "Подсказка про Кремль шаг 2", message: "Более детальное описание", commands: ["А", "B", "C", "Следующий шаг2"] },
+        "New": { message: "Привет я квет по городу Туле. Для начала квеста скажи где ты находишься?", commands: ["Кремль", "Набережная", "Все варианты"] },
+        "Old": { message: "Привет рад твоему возврашению. Ты можешь продолжить квест или начать с новой точки.", commands: ["Продолжить", "Кремль", "Все варианты"] },
+        "Продолжить": { button: { title: "Продолжить" },message: "Возобновление сессии", continue: true },
+        "Все варианты": { button: { title: "Все варианты" }, message: "Карта", commands: [], },
+        "Кремль": { button: { title: "Кремль" }, message: "История кремля. Задание 1. Варианты ответов", commands: ["A1", "B1", "C1", "Подсказка1"] },
+        "A1": { message: "Неправильно", commands: ["B1", "C1", "Подсказка1"], button: { title: "Вариант 1" } },
+        "B1": { message: "Неправильно", commands: ["A1", "C1", "Подсказка1"], button: { title: "Вариант 2" } },
+        "C1": { message: "Правильно.Нажми на кнопку чтобы получить новое задание.", commands: ["Следsующий шаг2"], button: { title: "Вариант 3" } },
+        "Подсказка1": { message: "Более детальное описание", commands: ["А1", "B1", "C1", "Следующий шаг2"], button: { title: "Подсказка" } },
+        "Следующий шаг2": {button: { title: "Следующие задание" }, message: "Задание 2. Варианты ответов", commands: ["А2", "B2", "C2", "Подсказка"] },
+        "Подсказка2": { message: "Более детальное описание", commands: ["А", "B", "C", "Следующий шаг2"] },
     }
 
 function readReq({ command, userState }) {
-    console.log(command, userState);
+    console.log("command, userState", command, userState);
     const
         { commands } = userState,
         result = commands
             .map((commandId) => commandDefinitions[commandId])
-            .filter((сommandDef) => {console.log(сommandDef ); return сommandDef && command === сommandDef.command})[0];
+            .filter((сommandDef) => { return сommandDef && сommandDef.button && command === сommandDef.button.title })[0];
+    console.log("result", result);
+    if (!result || result.continue === true) {
+        return userState;
+    }
     return result;
 }
 
 function writeResp(req, { message, commands }) {
-    console.log(message, commands);
+    console.log("message, commands", message, commands);
     const
         buttons = commands
-            .map((commandId) => { console.log(commandId,commandDefinitions[commandId], commandDefinitions);
-                 return commandDefinitions[commandId] 
-                })
-            .map((commandDefinition) => { 
-                console.log(commandDefinition);
-                return commandDefinition.button; 
+            .map((commandId) => {
+                return commandDefinitions[commandId]
+            })
+            .filter(commandDefinition => commandDefinition && commandDefinition.button)
+            .map((commandDefinition) => {
+                console.log("commandDefinition:", commandDefinition);
+                return commandDefinition.button;
             })
     return {
         version: req.body.version,
@@ -92,7 +79,7 @@ function writeResp(req, { message, commands }) {
 }
 
 app.get('/hello', function (req, res) {
-    res.send("test4");
+    res.send("test6");
 })
 
 app.use('*', function (req, res) {
